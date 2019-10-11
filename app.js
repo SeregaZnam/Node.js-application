cab2dd17a0f1ed8d60ef7bcf6917ec66e68c82cb
SeregaZnam <Sergey-znamensk@yandex.ru>
2019-10-11 19:04:56 +0300

    edit packages

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-import {DirWatcher} from './main/dir-watcher';
-import {Importer} from './main/importer';
+// import {DirWatcher} from './main/dir-watcher';
+// import {Importer} from './main/importer';
 
-const watcher = new DirWatcher();
-watcher.watch('./data');
+// const watcher = new DirWatcher();
+// watcher.watch('./data');
 
-const importer = new Importer(watcher);
+// const importer = new Importer(watcher);
 
-importer.import()
-	.then(d => console.log(d))
-	.catch(e => console.log(e));
+// importer.import()
+// 	.then(d => console.log(d))
+// 	.catch(e => console.log(e));
